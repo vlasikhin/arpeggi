476c263c574ddc3c1ccc4106239de7ecc3e75484
vlasikhin <vlasikhin@users.noreply.github.com>
2016-02-15 13:35:22 +0300

    Add username to divise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215102057) do
+ActiveRecord::Schema.define(version: 20160215102852) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160215102057) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "user_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
