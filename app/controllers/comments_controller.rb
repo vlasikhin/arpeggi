3c411766e914414753537946dc820646f66fbd01
vlasikhin <vlasikhin@users.noreply.github.com>
2016-02-16 11:03:06 +0300

    add "add comment action" with ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,12 +6,15 @@ def create
   @comment.user_id = current_user.id
 
   if @comment.save
-    flash[:success] = "You commented the hell out of that post!"
-    redirect_to :back
+    respond_to do |format|
+        format.html {redirect_to root_path}
+        format.js
+      end
   else
     flash[:alert] = "Check the comment form, something went horribly wrong."
     render root_path
   end
+  
 end
 
 def destroy  
